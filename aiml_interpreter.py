11a9264bd4cc38ad7af27f3fa794a9c221c0b2c6
Bam0jo <lukenorsk123@hotmail.com>
2022-08-23 15:08:15 +0200

    first commit

---
+++ aiml_interpreter.py
@@ -0,0 +1,65 @@
+#Check that aiml for python is installed through pip
+try:
+    import aiml
+except ModuleNotFoundError:
+    print("ERROR: AIML interpreter not installed")
+    print("Install by typing:") 
+    print('\tpython3 -m pip install python-aiml')
+    exit(1)
+
+#Import sys so that we can read filename from the commandline
+import sys
+
+if __name__ == '__main__':
+    #Check that a filename is specified
+    if len(sys.argv) < 2:
+        print('ERROR: No file specified')
+        print(f'Usage: python3 {sys.argv[0]} [name of aiml file]')
+        exit(1)
+    #Extract file name from command line argument
+    filename = sys.argv[1]
+
+    #The aiml library has bad error checking so lets check that 
+    #the file actually exists
+    try:
+        open(filename, 'r')
+    except FileNotFoundError:
+        print(f"ERROR: No file found with name {filename}")
+        exit(1)
+        
+    #Create instance of aiml kernel
+    k = aiml.Kernel()
+    #Have kernel read from file
+    k.learn(filename)
+    
+    #Run the actuall chatbot
+    done = False
+    print("Write questions for the chatbot to answer")
+    print("To quit enter 'q'")
+    while not done:
+        #Store pattern
+        pattern = input("> ")
+        #Check if user wants to quit
+        if pattern.lower() in ['q', 'quit']:
+            done = True
+            continue
+        response = k.respond(pattern)
+        print(response)
+    exit(0)
+
+
+
+
+
+
+
+
+
+#-----Notater-----#
+"""
+Åpne et program/video/bilde
+En form for web-searcher
+Lage et search query som programmet kan bruke
+Stille et spørsmål tilbake, så kan den spørre om man vil ha mer informasjon, deretter åpne en wikipedia side for mer info
+
+"""
